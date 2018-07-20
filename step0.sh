1c61735f7da2d3b09a0caddc72ed746ba72a4b47
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-07-20 16:37:22 +0200

    - embed binaries for all non-docker apps - improve script to run nomad and consul

--- step0.sh
+++ step0.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
-docker run -d --net=host --name=consul -e CONSUL_UI_BETA=true consul agent -server -ui -bootstrap-expect=1 -bind=192.168.63.21
-nomad agent -config server.hcl
+mkdir -p ./jalgoarena-data
+echo "jalgoarena-data dir created"
+
+CONSUL_UI_BETA=true nohup ./bin/consul agent -server -bootstrap-expect=1 -bind=192.168.63.21 -ui -data-dir=./jalgoarena-data/consul > logs/consul.out 2> logs/consul.err < /dev/null &
+echo "Consul started, check http://localhost:8500"
+./bin/consul version
+
+nohup ./bin/nomad agent -server -bootstrap-expect=1 -data-dir=$(pwd)/jalgoarena-data/nomad -client > logs/nomad.out 2> logs/nomad.err < /dev/null &
+echo "Nomad Started, check http://localhost:4646"
+./bin/nomad version
