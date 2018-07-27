0cc3c00a81961dd5ca240d95c52a3e74bab1604e
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-07-27 07:11:04 +0200

    - capture submissions table

--- nomad_raw_exec/jalgoarena.sql
+++ nomad_raw_exec/jalgoarena.sql
@@ -17,3 +17,22 @@ CREATE TABLE users (
 	UNIQUE INDEX uk_r43af9ap4edm43mmtq01oddj6 (username ASC),
 	FAMILY "primary" (id, email, "name", password, region, role, surname, team, username)
 );
+
+CREATE TABLE submissions (
+	id INTEGER NOT NULL,
+	consumed_memory BIGINT NOT NULL,
+	elapsed_time DOUBLE PRECISION NOT NULL,
+	error_message STRING(255) NULL,
+	failed_test_cases INTEGER NULL,
+	passed_test_cases INTEGER NULL,
+	problem_id STRING(255) NOT NULL,
+	source_code STRING(255) NOT NULL,
+	status_code STRING(255) NOT NULL,
+	submission_id STRING(255) NOT NULL,
+	submission_time STRING(255) NOT NULL,
+	token STRING(255) NULL,
+	user_id STRING(255) NOT NULL,
+	CONSTRAINT "primary" PRIMARY KEY (id ASC),
+	UNIQUE INDEX uk_51d698q9pdvfldc75kskyxmlf (submission_id ASC),
+	FAMILY "primary" (id, consumed_memory, elapsed_time, error_message, failed_test_cases, passed_test_cases, problem_id, source_code, status_code, submission_id, submission_time, token, user_id)
+);
