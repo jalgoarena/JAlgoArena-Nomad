fc85a0a6d2a403a541230dcf263c497c75bc584d
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-08-02 11:18:46 +0200

    - externalize configuration, prepare default one for upload to consul

--- nomad_raw_exec/step0.sh
+++ nomad_raw_exec/step0.sh
@@ -7,6 +7,9 @@ nohup consul agent -server -bootstrap-expect=1 -ui -bind=192.168.63.21 -data-dir
 echo "Consul started, check http://localhost:8500"
 consul version
 
+sleep 5
+consul kv put jalgoarena/config @config.json
+
 nohup nomad agent -server -bootstrap-expect=1 -data-dir=$(pwd)/jalgoarena-data/nomad -client -config=$(pwd)/nomad-client.hcl > logs/nomad.out 2> logs/nomad.err < /dev/null &
 echo "Nomad Started, check http://localhost:4646"
 nomad version
--- step0.sh
+++ step0.sh
@@ -8,7 +8,7 @@ echo "Consul started, check http://localhost:8500"
 ./bin/consul version
 
 sleep 5
-consul kv put jalgoarena/config @config.json
+./bin/consul kv put jalgoarena/config @config.json
 
 nohup ./bin/nomad agent -server -bootstrap-expect=1 -data-dir=$(pwd)/jalgoarena-data/nomad -client -config=$(pwd)/nomad-client.hcl > logs/nomad.out 2> logs/nomad.err < /dev/null &
 echo "Nomad Started, check http://localhost:4646"
