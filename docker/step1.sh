1393b847cf1f6161791e707f8344ce6f32b40e44
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-08-02 16:51:06 +0200

    - add missing cockroach job run

--- docker/step1.sh
+++ docker/step1.sh
@@ -4,3 +4,4 @@ nomad job run jalgoarena-logstash.nomad
 nomad job run jalgoarena-kibana.nomad
 nomad job run jalgoarena-kafka.nomad
 nomad job run jalgoarena-traefik.nomad
+nomad job run jalgoarena-cockroach.nomad
