54e3da8a0df8a39613ef357aa9d47816dca63743
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-08-06 19:03:53 +0200

    - improve echo message

--- nomad_consul/nomad_consul_server.sh
+++ nomad_consul/nomad_consul_server.sh
@@ -3,13 +3,13 @@ mkdir -p ./jalgoarena-data
 mkdir -p ./logs
 echo "jalgoarena-data & logs dir created"
 
-nohup consul agent -server -bootstrap-expect=1 -bind=192.168.63.21 -ui -data-dir=./jalgoarena-data/consul > logs/consul.out 2> logs/consul.err < /dev/null &
-echo "Consul started, check http://localhost:8500"
+nohup consul agent -server -bootstrap-expect=1 -bind=192.168.63.21 -client="192.168.63.21 127.0.0.1" -ui -data-dir=./jalgoarena-data/consul > logs/consul.out 2> logs/consul.err < /dev/null &
+echo "Consul started, check http://$(hostname):8500"
 consul version
 
 sleep 5
 consul kv put jalgoarena/config @config.json
 
 nohup nomad agent -server -bootstrap-expect=1 -data-dir=$(pwd)/jalgoarena-data/nomad -client -config=$(pwd)/nomad-client.hcl > logs/nomad.out 2> logs/nomad.err < /dev/null &
-echo "Nomad Started, check http://localhost:4646"
+echo "Nomad Started, check http://$(hostname):4646"
 nomad version
